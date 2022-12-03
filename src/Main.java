64f2c93005312316a1a225563c4d028950dfe3c8
Sanya Grigoryan <sgrigoryan@prolevtech.com>
2022-12-03 17:16:22 +0400

    first homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("");
+
+        int x = 7;
+
+        if (x==7) {
+            System.out.println("mi tesak en chi sax");
+        } else if (x>=0) {
+            System.out.println("ay esi tpi");
+        } else if (x<0) {
+            System.out.println("minusavoya");
+        } else System.out.println("500 error");
     }
 }
